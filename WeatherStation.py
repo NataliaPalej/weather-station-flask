c88714c8da8361e71ba7db486a2f231865e06aa2
Natalia Palej <palejnatalia@gmail.com>
2021-12-14 23:36:26 +0000

    Designed working code in Python using Flask library, half of documentation completed

--- WeatherStation.py
+++ WeatherStation.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template
-from sense_hat import SenseHat
+# from sense_hat import SenseHat
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    sense = SenseHat()
+    celsius = 20.2
+    fahrenheit = 68.4
+    humidity = 66.8824591688
+    pressure = 998.967125938
 
-    celsius = round(sense.get_temperature(), 1)
-    fahrenheit = round(1.8 * celsius + 32, 1)
-    humidity = sense.get_humidity_from_humidity()
-    pressure = sense.get_pressure_from_pressure()
-    h = sense.get_humidity()
-    p = sense.get_pressure()
+    # sense = SenseHat()
+    #
+    # celsius = round(sense.get_temperature(), 1)
+    # fahrenheit = round(1.8 * celsius + 32, 1)
+    # humidity = sense.get_humidity()
+    # pressure = sense.get_pressure()
 
-    return render_template('weather.html', celsius=celsius, fahrenheit=fahrenheit, pressure=pressure, humidity=humidity)
+    return render_template('weather.html', celsius=celsius,
+                           fahrenheit=fahrenheit, pressure=pressure,
+                           humidity=humidity)
 
 
 if __name__ == '__main__':
